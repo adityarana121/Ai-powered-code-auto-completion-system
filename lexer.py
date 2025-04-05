47dda2251416b2cd6d52e40458f6d489872d06dd
Disha <jaindisha155@gmail.com>
2025-04-05 21:38:26 +0530

    Added working lexer and parser

---
+++ lexer.py
@@ -0,0 +1,41 @@
+import ply.lex as lex
+
+# Token list
+tokens = (
+    'IDENTIFIER',
+    'NUMBER',
+    'PLUS',
+    'MINUS',
+    'MULT',
+    'DIV',
+    'EQUALS',
+)
+
+t_PLUS = r'\+'
+t_EQUALS = r'='
+t_ignore = ' \t'
+
+def t_IDENTIFIER(t):
+    r'[a-zA-Z_][a-zA-Z0-9_]*'
+    return t
+
+def t_NUMBER(t):
+    r'\d+'
+    t.value = int(t.value)
+    return t
+
+def t_newline(t):
+    r'\n+'
+    t.lexer.lineno += len(t.value)
+
+def t_error(t):
+    print("Illegal character '%s'" % t.value[0])
+    t.lexer.skip(1)
+
+lexer = lex.lex()
+
+data = 'x = 5 + 10'
+lexer.input(data)
+
+for tok in lexer:
+    print(tok)
---
+++ parser.py
@@ -0,0 +1,37 @@
+import ply.yacc as yacc
+from lexer import tokens 
+
+def p_statement_expr(p):
+    'statement : expression'
+    p[0] = ('stmt', p[1])
+
+def p_expression_binop(p):
+    '''expression : expression PLUS expression
+                  | expression MINUS expression
+                  | expression MULT expression
+                  | expression DIV expression'''
+    p[0] = ('binop', p[2], p[1], p[3])
+
+def p_expression_number(p):
+    'expression : NUMBER'
+    p[0] = ('num', p[1])
+
+def p_expression_identifier(p):
+    'expression : IDENTIFIER'
+    p[0] = ('var', p[1])
+
+def p_expression_assignment(p):
+    'expression : IDENTIFIER EQUALS expression'
+    p[0] = ('assign', p[1], p[3])
+
+def p_error(p):
+    print("Syntax error!")
+
+parser = yacc.yacc()
+
+# Test it
+if __name__ == "__main__":
+    code = "x = 5 + 3"
+    result = parser.parse(code)
+    print(result)
+
