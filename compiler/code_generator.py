ae9c744051907095a789279394bba9cd365ceea4
Disha <jaindisha155@gmail.com>
2025-04-05 22:54:02 +0530

    Add semantic analyzer and code generator

--- compiler/code_generator.py
+++ compiler/code_generator.py
@@ -1,1 +1,26 @@
 
+class CodeGenerator:
+    def __init__(self):
+        self.temp_count = 0
+        self.code = []
+
+    def new_temp(self):
+        self.temp_count += 1
+        return f"t{self.temp_count}"
+
+    def generate(self, node):
+        if node['type'] == 'assign':
+            target = node['children'][0]['value']
+            expr = self.generate(node['children'][1])
+            self.code.append(f"{target} = {expr}")
+            return target
+        elif node['type'] == 'binop':
+            left = self.generate(node['children'][0])
+            right = self.generate(node['children'][1])
+            temp = self.new_temp()
+            self.code.append(f"{temp} = {left} {node['value']} {right}")
+            return temp
+        elif node['type'] == 'num':
+            return str(node['value'])
+        elif node['type'] == 'var':
+            return node['value']
--- compiler/semantic_analyzer.py
+++ compiler/semantic_analyzer.py
@@ -1,1 +1,13 @@
 
+class SemanticAnalyzer:
+    def __init__(self):
+        self.symbol_table = {}
+
+    def analyze(self, node):
+        if node['type'] == 'assign':
+            var_name = node['children'][0]['value']
+            self.symbol_table[var_name] = 'int'  # hardcoding type for now
+        elif node['type'] == 'var':
+            var_name = node['value']
+            if var_name not in self.symbol_table:
+                raise Exception(f"Undefined variable: {var_name}")
